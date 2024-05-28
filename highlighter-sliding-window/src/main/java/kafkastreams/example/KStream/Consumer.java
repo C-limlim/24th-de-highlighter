3f9b1d196be38bf276b0ddfa22ea7185e0fed96b
hengyinayo <wuheng@yonsei.ac.kr>
2024-05-28 18:09:24 +0900

    feat: video Download API gateway connection and mongodb connection

--- highlighter-sliding-window/src/main/java/kafkastreams/example/KStream/Consumer.java
+++ highlighter-sliding-window/src/main/java/kafkastreams/example/KStream/Consumer.java
@@ -1,5 +1,9 @@
 package kafkastreams.example.KStream;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -7,6 +11,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.bson.Document;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -18,12 +23,17 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class Consumer {
-    private final static String API_GATEWAY_URL_BASE = "https://pjo1n4hxlg.execute-api.ap-northeast-2.amazonaws.com/default/messageConsumer?start=";
+    private final static String API_GATEWAY_URL_BASE = "https://jgr795jq6l.execute-api.ap-northeast-2.amazonaws.com/default/triggerRequest?start=";
     private final static Logger log = LoggerFactory.getLogger(Consumer.class);
     private final static String BOOTSTRAP_SERVERS = "43.203.141.74:9092";  /* change ip */
     private final static String GROUP_ID = "kstream-application";  /* this can be anything you want */
     private final static String TOPIC_NAME = "stream_filter_sink";
-    //private final static String FASTAPI_URL = "http://192.168.176.2:8000/title_creation/";
+
+    private final static String MONGODB_URI = "mongodb://ec2-43-203-141-74.ap-northeast-2.compute.amazonaws.com:27017";
+    private final static String DATABASE_NAME = "youtube_hightlight";
+    private final static String COLLECTION_NAME = "youtube_hightlight_title";
+
+
 
     public Consumer() {}
     public static void main(String[] args) {
@@ -54,7 +64,8 @@ public class Consumer {
 
                     // API Gateway 로 전송
                     sendKeyToApiGateway(record.value());
-                    //sendKeyToFastAPI(record.key());
+                    // Save to MongoDB
+                    //saveToMongoDB(record.value());
                 }
             }
         } catch (Exception e) {
@@ -82,11 +93,7 @@ public class Consumer {
             // key: "Threshold exceeded from 2024-05-28 04:31:09 to 2024-05-28 04:31:28"
             String[] parts = key.split(" to ");
             String startFull = parts[0].trim();
-            log.info(startFull);
             String endFull = parts[1].trim();
-            // Removing spaces from the end date
-            log.info(endFull);
-
             String urlStr = API_GATEWAY_URL_BASE + startFull + "&end=" + endFull;
             urlStr = urlStr.replace(" ", "%20");
             log.info(urlStr);
@@ -94,33 +101,6 @@ public class Consumer {
 
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json; utf-8");
-                connection.setRequestProperty("Accept", "application/json");
-            connection.setDoOutput(true);
-
-            String jsonInputString = "{\"key\": \"" + key + "\"}";
-
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-                os.write(input, 0, input.length);
-            }
-
-            int responseCode = connection.getResponseCode();
-            log.info("Response Code from API Gateway: " + responseCode);
-
-            connection.disconnect();
-        } catch (Exception e) {
-            log.error("Error sending key to API Gateway", e);
-        }
-    }
-
-
-  /*
-    private void sendKeyToFastAPI(String key) {
-        try {
-            URL url = new URL(FASTAPI_URL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json; utf-8");
             connection.setRequestProperty("Accept", "application/json");
             connection.setDoOutput(true);
@@ -141,5 +121,24 @@ public class Consumer {
         }
     }
 
-   */
+    private void saveToMongoDB(String value) {
+        try {
+
+            MongoClient mongoClient = MongoClients.create(MONGODB_URI);
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+
+            String[] parts = value.split(" to ");
+            String startFull = parts[0].trim();
+            String endFull = parts[1].trim();
+
+            Document doc = new Document("start", startFull)
+                    .append("end", endFull);
+            collection.insertOne(doc);
+            log.info("Successfully inserted document into MongoDB");
+        } catch (Exception e) {
+            log.error("Error inserting document into MongoDB", e);
+        }
+    }
+
 }
